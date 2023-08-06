797211f046268e356de69be9617a883d77c35ae0
NguyenHuuPhuc <nguyenhuuthuc0405@gmail.com>
2023-08-06 21:02:38 +0700

    MAKE HUGE CHANGE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import Appbar from './component/Appbar'
-import Member from './component/Member';
-import MemberTable from './component/MemberTable'
+import React from 'react';
+import { useEffect } from 'react'
+import jwt_decode from "jwt-decode"
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            <Appbar/>
-            <Member/>
-            <MemberTable/>
-        </div>
-    );
-}
 
+  function handleCallbackResponse(response){
+    console.log("TOKEN: " + response.credential)
+    var userObject = jwt_decode(response.credential)
+    console.log(userObject)
+  }
+
+  useEffect(() =>{
+    google.accounts.id.initialize({
+      client_id: "155692422040-jt9bej9soiqtdqkp97fq57f55i7le10p.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    )
+
+  }, []);
+
+  return (
+    <div>
+      <div id = "signInDiv"></div>
+    </div>
+  )
+
+}
 export default App;
